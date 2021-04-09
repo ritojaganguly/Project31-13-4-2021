d836af377fbf6cd9b29d3734f032755ca9a6f3c7
ritojaganguly <78806021+ritojaganguly@users.noreply.github.com>
2021-04-09 22:28:22 +0530

    Add files via upload

---
+++ constraint.js
@@ -0,0 +1,16 @@
+class Throw{
+    constructor(bodyA,pointB){
+        var options={
+            bodyA : bodyA,
+            pointB : pointB,
+            stiffness : 0.004,
+            length : 10
+        }
+        this.pointB=pointB;
+        this.throw=Constraint.create(options);
+        World.add(world, this.throw);
+    }
+    fly(){
+        this.throw.bodyA=null;
+    }
+}
